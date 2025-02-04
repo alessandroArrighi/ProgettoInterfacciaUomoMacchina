c3906d05ad58ef85f68b497180e47b375270daf0
filippo16 <filippo.lutterotti2@gmail.com>
2025-02-04 17:24:04 +0100

    Add register

---
+++ ProgettoHMI.web/Features/Register/RegisterController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProgettoHMI.web.Features.Register
+{
+    public partial class RegisterController : Controller
+    {
+        [HttpGet]
+        public virtual IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
