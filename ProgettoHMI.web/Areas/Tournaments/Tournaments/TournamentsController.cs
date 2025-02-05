c193a4b9a0457b7ed24cd8ae1b4be21b7db8eee1
filippo16 <filippo.lutterotti2@gmail.com>
2025-02-05 12:01:27 +0100

    Init Tournaments dir

---
+++ ProgettoHMI.web/Areas/Tournaments/Tournaments/TournamentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProgettoHMI.web.Areas.Tournaments.Tournaments
+{
+    public class TournamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
