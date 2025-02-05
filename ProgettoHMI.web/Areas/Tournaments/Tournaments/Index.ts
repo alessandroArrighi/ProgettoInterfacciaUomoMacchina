e829b427821c65e0007e97c148d64dd785602f3c
alessandroArrighi <alessandro.arrighi2@studio.unibo.it>
2025-02-05 21:41:31 +0100

    index tornei

---
+++ ProgettoHMI.web/Areas/Tournaments/Tournaments/Index.ts
@@ -0,0 +1,10 @@
+﻿module Tournaments.Tournaments {
+    export class indexViewModel {
+        public model: Tournaments.Server.IndexViewModelInterface;
+        public prova: string;
+
+        public constructor(model: Tournaments.Server.IndexViewModelInterface) {
+            this.model = model;
+        }
+    }
+}
---
+++ ProgettoHMI.web/Areas/Tournaments/Tournaments/IndexViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using ProgettoHMI.web.Infrastructure;
+
+namespace ProgettoHMI.web.Areas.Tournaments.Tournaments
+{
+    [TypeScriptModule("Tournaments.Tournaments.Server")]
+    public class IndexViewModel
+    {
+        public IEnumerable<TournamentListViewModel> Tournaments { get; set; }
+
+        public IndexViewModel(string name, string rank)
+        {
+            this.Tournaments = new List<TournamentListViewModel>
+            {
+                new TournamentListViewModel
+                {
+                    Name = name,
+                    Rank = rank
+                },
+                new TournamentListViewModel
+                {
+                    Name = "provaName2",
+                    Rank = "Gold"
+                },
+                new TournamentListViewModel
+                {
+                    Name = "provaName3",
+                    Rank = "Silver"
+                },
+                new TournamentListViewModel
+                {
+                    Name = "provaName4",
+                    Rank = "Bronze"
+                },
+                new TournamentListViewModel
+                {
+                    Name = "provaName5",
+                    Rank = "Gold"
+                },
+                new TournamentListViewModel
+                {
+                    Name = "provaName6",
+                    Rank = "Diamond"
+                }
+            };
+        }
+
+        public string ToJson()
+        {
+            return Infrastructure.JsonSerializer.ToJsonCamelCase(this);
+        }
+    }
+
+    [TypeScriptModule("Tournaments.Tournaments.Server")]
+    public class TournamentListViewModel
+    {
+        public string Name { get; set; }
+        public string Rank { get; set; }
+    }
+}
