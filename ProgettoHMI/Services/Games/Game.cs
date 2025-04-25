1e0930188cde6feee5336312c0bf91f6937039c9
alessandroArrighi <alessandro.arrighi2@studio.unibo.it>
2025-04-25 16:05:55 +0200

    add Game model

---
+++ ProgettoHMI/Services/Games/Game.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Dynamic;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
+using ProgettoHMI.Services.Shared;
+
+namespace ProgettoHMI.Services.Games
+{
+    public class Game
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid GameId { get; set; }
+        public Guid TournamentId { get; set; }
+        public int DrawPosition {get; set; }
+        public string Status { get; set; }
+        public User User1 { get; set; }
+        public User User2 { get; set; }
+        public Score Score { get; set; }
+    }
+
+    public class Score {
+        private const int LEN = 5;
+        public ScoreSet?[] Set;
+
+        public Score(ScoreSet[] sets) {
+            Set = new ScoreSet?[LEN];
+
+            for(int i = 0; i < Set.Length && i < LEN; ++i) {
+                Set[i] = sets[i];
+            }
+        }
+    }
+
+    public struct ScoreSet {
+        public int Score1;
+        public int Score2;
+
+        public ScoreSet(int S1, int S2) {
+            Score1 = S1;
+            Score2 = S2;
+        }
+    }
+}
