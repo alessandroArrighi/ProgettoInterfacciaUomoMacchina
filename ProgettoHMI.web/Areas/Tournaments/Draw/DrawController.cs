854d16dfff14f6b3ec8f6c4d2762c8ef6260593e
filippo16 <filippo.lutterotti2@gmail.com>
2025-05-28 20:14:16 +0200

    Perform card

---
+++ ProgettoHMI.web/Areas/Tournaments/Draw/DrawController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProgettoHMI.web.Areas.Tournaments.Draw
+{
+    [Area("Tournaments")]
+    public partial class DrawController : Controller
+    {
+        public virtual IActionResult Draw()
+        {
+            return View();
+        }
+    }
+}
